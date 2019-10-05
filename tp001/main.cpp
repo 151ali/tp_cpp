69fefc0bccc74a3cedc56f1219531bbeaee41612
َAli IKHLEF <39748592+151ali@users.noreply.github.com>
2019-10-06 00:30:12 +0200

    Create main.cpp

---
+++ tp001/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "sort.h"
+
+using namespace std;
+
+int main()
+{
+
+    int t[9] = {1 ,5 ,8 ,7 ,9 ,4 ,3 ,2 ,6};
+
+
+   // ArrayOut(t,9);
+   // SelectionSort(t,9);
+   // InsertionSort(t,9);
+   // BubbleSort(t,9);
+
+   // ArrayOut(t,9);
+   // cout << ArraySum(t,0,2);
+
+
+   // cout << SearchElement(t,9,9) << endl; // will print 4
+   // cout << SearchElement(t,9,151) << endl; // will print -
+
+
+   // cout << MaxVect(t,0,5) << endl; // will print 9
+   // cout << MinVect(t,0,5) << endl; // will print 1
+
+
+   // cout << MinMaxVect(t,0,5);      // will print 1 if ur answer -1 ,,,  will print 9 if ur answer 1
+
+
+
+
+
+
+
+
+    return 0;
+}
